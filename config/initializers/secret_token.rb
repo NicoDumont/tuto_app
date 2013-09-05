b5ed24a545f28056882c68e352d26fba9bc8ade3
vagrant <vagrant@simruby-devbox>
2013-09-05 09:35:49 +0000

    depot initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TutoApp::Application.config.secret_key_base = '25a6f50e0213b7ac49c4e1d4f71845cfeb4d1fb4c40a95d867b4c529433ed66bc8de8d2fd5ee3ce14ef2a2939564a4fd7277c361d80fff936dc6854eecb718b9'
